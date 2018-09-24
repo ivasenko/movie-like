1fc105d195efdfb46dede546035694cc1d2d9837
Iren <ivasenko.iren@gmail.com>
2018-09-24 13:56:06 +0300

    new-tamplate v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
