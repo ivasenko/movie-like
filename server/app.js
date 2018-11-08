4f590b763b7353924f21e47182cf257e76f775a2
Iren <ivasenko.iren@gmail.com>
2018-11-08 21:39:46 +0200

    prettier

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const express = require("express");
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -11,18 +11,18 @@ app.use(express.static(path.join(__dirname, '../build'), { maxAge: 86400000 }));
 app.use(bodyParser());
 
 //handle any errors
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.send(500, 'Something broke!');
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
 });
 
 // Render the app
 app.get('/', function(req, res) {
-    res.sendfile(path.join(__dirname, '../build/index.html'));
+  res.sendfile(path.join(__dirname, '../build/index.html'));
 });
 
 //start server
 const port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
